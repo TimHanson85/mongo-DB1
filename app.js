990d2f38efdd075bbbd5d8a54410a48c2cf19026
ifics <blakejohnson.pdx@gmail.com>
2015-06-10 15:01:12 -0700

    working base mongo server and shell

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var users = require('./routes/users');
 
 var app = express();
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://127.0.0.1:27017/test';
+
+  MongoClient.connect(url, function(err, db){
+    if (err) throw err;
+    app.set('mongo', db); 
+    console.log("Connected to mongo");
+  });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
