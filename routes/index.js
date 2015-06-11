0f96b07e87baa77a57bdc3cb0297c6c79f63e671
ifics <blakejohnson.pdx@gmail.com>
2015-06-11 11:36:46 -0700

    finished inserting into database, finding and exporting into info.jade, next step clicksCounter and timeStamp, 8===D

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var app = require('../app.js');
 var uuid = require('node-uuid');
-var mongo = app.get('mongo');
 
   
   //homepage handler++++++++++++++++++++
   router.get('/', function(request, response) {
+    var db = app.get('mongo'); 
     response.render('index', {});
     // index.jade needs a form to submit a URL for shortening
   });
@@ -14,26 +14,32 @@ var mongo = app.get('mongo');
 
   //post handler for submiting form+++++++++++
   router.post('/', function(request, response) {
-    var id = uuid.v4();
-    var userInput = document.body.url;
-    var collection = mongo.collection('urls');
-    collection.insert({/*info you generate*/}, function(err, docs) {
-      response.redirect('/info/' + shortUrl);
+    var db = app.get('mongo'); 
+    var shortUrl = uuid.v4();
+    var userInput = request.body.url;
+    var collection = db.collection('urls');
+    
+    collection.insert({
+      'shortened': shortUrl,
+      'target': userInput}, function(err, docs) {
+    response.redirect('/info/' + shortUrl);
     });
   });
 
   //Get handler+++++++++++++++++++++
   router.get('/info/:shortUrl', function(request, response) {
-    var collection = mongo.collection('urls'),
-        shortUrl = request.params.shortUrl;
-    collection.find().toArray({'shortened': shortUrl}, function(err, url) {
-      response.render('info', {url: url});
+    var db = app.get('mongo'); 
+    var collection = db.collection('urls');
+    var shortUrl = request.params.shortUrl;
+    collection.find({'shortened': shortUrl}).toArray(function(err, url) {
+      response.render('info', {url: url[0]});
     });
   });
 
   //get handler for shotening url+++++++
   router.get('/:shortUrl', function(request, response) {
-    var collection = mongo.collection('urls'),
+    var db = app.get('mongo'); 
+    var collection = db.collection('urls'),
         shortUrl = request.params.shortUrl;
     collection.find({'shortened': shortUrl}, function(err, url) {
       response.redirect(url.target);
@@ -43,14 +49,8 @@ var mongo = app.get('mongo');
   */
 
 
-{
-  "_id": id,
-  "shortened": shortUrl,
-  "target": userInput,
-  "clicks": 8,
-  "last_click": "2015-01-13T16:42:00"
-}
-
+//"clicks": 8
+//"last_click": "2015-01-13T16:42:00"
 
 /*
 ************************************/
